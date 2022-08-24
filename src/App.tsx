2396aec40ae6d490c63dcde76610551422fc14d0
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-24 14:06:13 -0500

    Add Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,42 @@
+
+const Container = styled.div`
+  font-family: 'Lato', sans-serif;
+  max-width: 1024px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 40px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto;
+  }
+`;
+
 const App = (): JSX.Element => {
   useEffect(() => {
     if (Object.values(trade).some(val => val === '' || !val)) return;
