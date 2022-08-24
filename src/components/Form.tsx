24803e9b6431a1efcee6d00d7f482992ea2cadd4
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-23 21:18:12 -0500

    Add styles to submit input

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,31 @@
+import styled from '@emotion/styled';
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #7A7DFE;
+    }
+`;
+
 const Form = (): JSX.Element => {
     return (
         <form>
 
-            <input type="submit" value="Trade"/>
+            <InputSubmit 
+                type="submit" 
+                value="Trade"
+            />
         </form>
     );
 }
