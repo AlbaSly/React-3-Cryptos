2ac32962cbd93c048e1035567a45be5fee40d320
AlbaSly <arias.brando.raxel@gmail.com>
2022-08-24 03:30:55 -0500

    Add Alert Component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,34 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+import styled from "@emotion/styled";
+
+import { AlertComponentProps } from "../interfaces/component.interface";
+
+const Info = styled.div`
+    background-color: #B7322C;
+    color: #FFF;
+    padding: 4px;
+    font-size: 22px;
+    text-transform: uppercase;
+    font-weight: 700;
+    text-align: center;
+    margin: 10px auto;
+`;
+
+export const Alert = (props: AlertComponentProps): JSX.Element => {
+    const {alertInfoState: {alertInfo, setAlertInfo}} = props;
+
+    useEffect(() => {
+        const show = setTimeout(() => {
+            setAlertInfo('');
+        }, 1000);
+
+        return (): void => clearTimeout(show);
+    }, [alertInfo]);
+
+    return (
+        <Info>
+            <p>{alertInfo}</p>
+        </Info>
+    );
+}
